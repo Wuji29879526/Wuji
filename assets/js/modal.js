fe54c3ddd2b0be225154f2352c65d9ae66d79e95
Wuji29879526 <pbb5stcz22@privaterelay.appleid.com>
2025-12-17 00:20:07 +0800

    Add files via upload

---
+++ assets/js/modal.js
@@ -0,0 +1,7 @@
+
+function openDanshen() {
+  document.getElementById('danshenModal').style.display = 'flex';
+}
+function closeDanshen() {
+  document.getElementById('danshenModal').style.display = 'none';
+}
